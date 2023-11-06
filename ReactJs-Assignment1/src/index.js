31840c420003957b85c44ef8f395f73789bd7326
Monika0123456789 <107780828+Monika0123456789@users.noreply.github.com>
2023-11-06 19:15:06 +0530

    Add files via upload

---
+++ ReactJs-Assignment1/src/index.js
@@ -0,0 +1,7 @@
+import React,{Component} from "react"
+import {createRoot} from "react-dom/client"
+import App from './componets/app'
+
+
+
+createRoot(document.getElementById("root")).render(<App/>)
